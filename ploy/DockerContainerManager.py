2e8fa5decd558f93e07c8d0f967c100381bbd829
Jasper @ Excalibur <vandenBosch@med.uni-frankfurt.de>
2013-12-30 16:06:05 -0800

    DockerContainerManager handles multiple containers, and can print stdout of container on exit.

--- ploy/DockerContainerManager.py
+++ ploy/DockerContainerManager.py
@@ -4,15 +4,20 @@ class DockerContainerManager(object):
 
     def __init__(self, dockerClient):
         self.client = dockerClient
-        self.container = None
+        self.containers = []
+        self.containersToPrintOutputFor = []
 
     def __enter__(self):
         return self
 
     def __exit__(self, *_):
-        if self.container:
-            self.client.kill(self.container)
-            self.client.remove_container(self.container)
+        for container in self.containersToPrintOutputFor:
+            self.client.logs(container)
+        for container in self.containers:
+            self.client.kill(container)
+            self.client.remove_container(container)
 
-    def add(self, container):
-        self.container = container
+    def add(self, container, printOutput=False):
+        self.containers.append(container)
+        if printOutput:
+            self.containersToPrintOutputFor.append(container)
--- tests/DockerContainerManagerTestCase.py
+++ tests/DockerContainerManagerTestCase.py
@@ -9,12 +9,15 @@ class DockerContainerManagerTestCase(TestCase):
         from ploy.DockerContainerManager import DockerContainerManager
         client = Mock()
         with DockerContainerManager(client) as manager:
-            # When I add a container
+            # When I add some container
             manager.add('my_container')
+            manager.add('other_container')
             # and exit the context
         # the container is killed and removed
         client.kill.assert_any_call('my_container')
+        client.kill.assert_any_call('other_container')
         client.remove_container.assert_any_call('my_container')
+        client.remove_container.assert_any_call('other_container')
 
     def test_On_exit_without_containers_exits_silently(self):
         from ploy.DockerContainerManager import DockerContainerManager
@@ -24,4 +27,14 @@ class DockerContainerManagerTestCase(TestCase):
             pass
             # and exit the context
         # nothing happens
+
+    def test_On_exit_prints_logs_for_container_by_request(self):
+        from ploy.DockerContainerManager import DockerContainerManager
+        client = Mock()
+        with DockerContainerManager(client) as manager:
+            # When I add a container
+            manager.add('my_container', printOutput=True)
+            # and exit the context
+        # the container is killed and removed
+        client.logs.assert_any_call('my_container')
 
